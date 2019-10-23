13cfee1fa9acd1af375da860391551509a6d03d7
bcooley <brendan.cooley@gmail.com>
2019-10-23 17:51:08 -0400

    time trace minimization

--- chicago/03_analyze.py
+++ chicago/03_analyze.py
@@ -1,11 +1,15 @@
 import numpy as np
 import imp
+import time
 
 import helpers
 imp.reload(helpers)
 
 counts = np.genfromtxt("output/chi_dmatrix.csv", delimiter=",")
 covM = helpers.covMat(counts, zero=True)
+
+start_time = time.time()
 gammaL = helpers.traceMin(covM)
+print("trace minimization completed in %s seconds" % (time.time() - start_time))
 
 np.savetxt("output/gammaL.csv", gammaL, delimiter=",")
